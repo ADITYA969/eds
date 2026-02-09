54727cdc893d9b40da1b1077d0cfe55503b62927
aditya.nemalikonda <aditya.nemalikonda@tadigital.com>
2026-02-09 15:35:35 +0530

    updated blogs content

--- blocks/blogs/blogs.js
+++ blocks/blogs/blogs.js
@@ -3,29 +3,33 @@ export default function decorate(block) {
   blogsWrapper.classList.add('container');
   const blogsList = document.createElement('div');
   blogsList.classList.add('blogs-list');
-  const blogListItems = block.querySelectorAll('.blogs>div');
+
+  const blogListItems = block.querySelectorAll('.blogs > div');
+
   blogListItems.forEach((item) => {
     const blogItem = document.createElement('div');
     blogItem.classList.add('blog-item');
-    const blogTitle = item.querySelector("h3").textContent.trim();
-    const details = item.querySelectorAll("p");
+
+    const blogTitle = item.querySelector('h3').textContent.trim();
+    const details = item.querySelectorAll('p');
     const date = details[0].textContent.trim();
     const blogDescription = details[1].textContent.trim();
-    // const readMore = item.querySelector('a');
-    // const readMoreText = readMore.textContent;
-    // const readMoreLink = readMore.href;
+
     blogItem.innerHTML = `
-        <div class="blog-details">
-            <h3>${blogTitle}</h3>
-            <span>${date}</span>
-        </div>
-        <p>${blogDescription}</p>
+      <div class="blog-details">
+        <h3>${blogTitle}</h3>
+        <span>${date}</span>
+      </div>
+      <p>${blogDescription}</p>
     `;
+
     blogsList.append(blogItem);
   });
+
   blogsWrapper.append(blogsList);
 
   block.innerHTML = '';
   block.append(blogsWrapper);
-  console.log(block)
+
+  console.log(block);
 }
